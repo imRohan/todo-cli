c96206039ea179c92476824c1ec0ebcf837fc19f
Rohan Likhite <contact@rohanlikhite.com>
2020-05-25 09:59:55 -0400

    removed unused files for cli app

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,9 @@
 #!/usr/bin/env node
 // Get External Deps
-import bodyParser = require('body-parser')
-import cors = require('cors')
-import express = require('express')
 require('dotenv').config()
 
-// Routes
-import _sampleRoutes = require('./routes/sample')
+function start() {
+  console.log('hello')
+}
 
-// Express server setup and start
-const server = express()
-server.use(cors())
-server.use(bodyParser.json())
-server.use(express.static(__dirname))
-
-// Routes
-server.use('/api/sample', _sampleRoutes)
-
-server.get('/', (request, response) => {
-  response.sendFile('index.html', { root: process.env.PWD })
-})
-
-// Start the Express Server & Init the application
-const _serverInstance = server.listen(process.env.API_SERVER_PORT, () => {
-  console.log(`Server running on port ${process.env.API_SERVER_PORT}`)
-})
-
-process.on('SIGINT', () => {
-  _serverInstance.close()
-})
+start()
